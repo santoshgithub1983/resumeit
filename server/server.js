869c0b6a785cfc9f5043680132e4732a9bddd4b5
Santosh_Pandhare <santosh.pandhare@gmail.com>
2023-10-02 16:04:14 +0530

    Updated routes in serverjs - attempt 8

--- server/server.js
+++ server/server.js
@@ -7,17 +7,24 @@ const userRoute  = require('./routes/userRoute')
 const path = require('path')
 
 app.use('/api/user' , userRoute)
+app.use('/api/login' , userRoute)
+app.use('/api/register' , userRoute)
+app.use('/api/update' , userRoute)
+app.use('api/home' , userRoute)
+app.use('api/templates/:id' , userRoute)
+app.use('api/user/profile' , userRoute)
 
 if ( process.env.NODE_ENV === 'production')
 {
-    app.use('/', express.static("app/build"))
+   // app.use('/', express.static("app/build"))
     // Serve static files from the "build" directory
     app.use(express.static(path.join(__dirname, 'app/build')));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname , "app/build/index.html"))
-    })
+    app.get("*" , (req , res) => {
+        // res.sendFile(path.resolve(__dirname , "app/build/index.html"))
+        res.sendFile(path.join(__dirname, 'build', 'app/build/index.html'));
+    });
 }
 
-// app.get('/', (req, res) => res.send('Hi'))
+app.get('/', (req, res) => res.send('Hi'))
 app.listen(port, () => console.log(`This app is listening on port ${port}!`))
