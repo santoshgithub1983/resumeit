cf92eaf186d127f09f34a6ae8de6873ecd232c0e
Santosh_Pandhare <santosh.pandhare@gmail.com>
2023-10-09 19:05:08 +0530

    added Access-Control-Allow-Origin

--- functions/nodeserverfunction.js
+++ functions/nodeserverfunction.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
     // You can also configure other CORS headers as needed
     res.header('Access-Control-Allow-Origin', 'http://resumeit-santoshgithub1983-santoshgithub1983s-projects.vercel.app');
     res.header('Access-Control-Allow-Origin', 'http://resumeit-theta.vercel.app');
+    res.header('Access-Control-Allow-Origin', 'https://resumeit-p7eeegmv6-santoshgithub1983s-projects.vercel.app');
     next();
   });
 
